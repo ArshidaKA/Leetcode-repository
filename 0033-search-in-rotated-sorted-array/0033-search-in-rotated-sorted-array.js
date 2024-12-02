01d86ee1549887a85bfb07ff2ed8a8e6bf47c395
ArshidaKA <arshidaakbar39@gmail.com>
2024-12-02 10:38:46 +0530

    Time: 0 ms (100%), Space: 49.4 MB (20.65%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+
+        return nums.indexOf(target)
+
+    
+    }
+    
