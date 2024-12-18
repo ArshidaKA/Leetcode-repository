0a3840f722e80a20d10c436ee8c7b15968de2bd8
ArshidaKA <arshidaakbar39@gmail.com>
2024-12-18 10:10:20 +0530

    Time: 7 ms (52.86%), Space: 51 MB (79.06%) - LeetHub

---
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} target
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+
+var canBeEqual = function(target, arr) {
+    return target.sort((a, b) => a - b).join() === arr.sort((a, b) => a - b).join();
+};
