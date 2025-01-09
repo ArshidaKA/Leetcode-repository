3b0ecf41414807c22b2b60b797eac0b250236531
ArshidaKA <arshidaakbar39@gmail.com>
2025-01-09 10:39:42 +0530

    Time: 26 ms (33.43%), Space: 52.8 MB (55.73%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    let newS=s.split('').sort().join();
+     let newT=t.split('').sort().join();
+    
+    if(newS===newT){
+        return true
+    }else{
+       return  false
+    }
+    
+};
