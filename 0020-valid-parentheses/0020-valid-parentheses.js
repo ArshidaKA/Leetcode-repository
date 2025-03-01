f0a068ccf559fc14d4a2e95a0a5ec74bf6ff071a
ArshidaKA <arshidaakbar39@gmail.com>
2025-03-01 10:02:39 +0530

    Time: 2 ms (76.1%), Space: 54.2 MB (20.62%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+function isValid(s) {
+    let stack = [];
+    let bracketMap = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let char of s) {
+        if (char in bracketMap) {
+            let topElement = stack.length ? stack.pop() : '#';
+            if (topElement !== bracketMap[char]) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
+}
